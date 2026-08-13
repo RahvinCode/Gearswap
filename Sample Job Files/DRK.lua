b98d85854d4620ce8066833bcd07dd15ca102aba
Rahvin <rahvincode@gmail.com>
2026-08-13 15:46:48 -0600

    Update DRK.lua

--- Sample Job Files/DRK.lua
+++ Sample Job Files/DRK.lua
@@ -412,7 +412,7 @@ end
 
 --Function used to automate Job Ability use
 function check_buff_JA()
-	buff = 'None'
+	local buff = 'None'
 	local ja_recasts = windower.ffxi.get_ability_recasts()
 
 	if player.sub_job == 'SAM' and player.sub_job_level == 49 then
@@ -437,7 +437,7 @@ function check_buff_JA()
 end
 
 function check_buff_SP()
-	buff = 'None'
+	local buff = 'None'
 	--local sp_recasts = windower.ffxi.get_spell_recasts()
 	return buff
 end
